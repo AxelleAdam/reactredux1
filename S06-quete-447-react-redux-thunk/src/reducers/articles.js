9e62b83aae90e1f92075ad227dd12e5fe02b2700
AxelleAdam <47389976+AxelleAdam@users.noreply.github.com>
2019-06-03 17:01:00 +0200

    Update articles.js

--- S06-quete-447-react-redux-thunk/src/reducers/articles.js
+++ S06-quete-447-react-redux-thunk/src/reducers/articles.js
@@ -1,19 +1,19 @@
 const articles = (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_ARTICLES__RESOLVE':
-      return action.articles.map(article => ({
-        ...article,
-        isSelected: false,
-      }));
-    case 'TOGGLE_ARTICLE_SELECTION':
-      return state.map(article =>
-        article.name === action.articleName
-          ? { ...article, isSelected: !article.isSelected }
-          : article
-      );
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'FETCH_ARTICLES__RESOLVE':
+			return action.articles.map(article => ({
+				...article,
+				isSelected: false,
+			}));
+		case 'TOGGLE_ARTICLE_SELECTION':
+			return state.map(article =>
+				article.name === action.articleName
+					? { ...article, isSelected: !article.isSelected }
+					: article
+			);
+		default:
+			return state;
+	}
 };
 
 export default articles;
