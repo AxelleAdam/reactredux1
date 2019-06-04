5bcc53e3fe78b1d181815cff03502e2a2caf3f70
AxelleAdam <47389976+AxelleAdam@users.noreply.github.com>
2019-06-04 17:29:32 +0200

    Update areArticlesBeingFetch.js

--- S06-quete-447-react-redux-thunk/src/reducers/areArticlesBeingFetch.js
+++ S06-quete-447-react-redux-thunk/src/reducers/areArticlesBeingFetch.js
@@ -1,13 +1,12 @@
-
 const areArticlesBeingFetched = (state = false, action) => {
-	switch (action.type) {
-		case 'FETCH_ARTICLES__BEGIN':
-			return true;
-		case 'FETCH_ARTICLES__RESOLVE':
-			return false;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'FETCH_ARTICLES__BEGIN':
+      return true;
+    case 'FETCH_ARTICLES__RESOLVE':
+      return false;
+    default:
+      return state;
+  }
 };
 
 export default areArticlesBeingFetched;
